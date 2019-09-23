5b31c58363d86844808d12be8cf4b26645507872
Doddler <doddler@gmail.com>
2019-09-23 00:00:47 -0600

    Committing the goods

---
+++ Program.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.IO;
+using System.Reflection;
+
+namespace ExePatchPreparer
+{
+    class Program
+    {
+        struct ExeSection
+        {
+            public string Name;
+            public string Code;
+            public int VOffset;
+            public int VSize;
+            public int ROffset;
+            public int RSize;
+            public uint Flags;
+        }
+
+        public static int RoundToAlignment(int address, int align)
+        {
+            var remainder = address % align;
+            if (remainder == 0)
+                return address;
+            return address - remainder + align;
+        }
+
+        public static (bool success, string error) CreateExePatchFile(string exePath, string asmPath)
+        {
+            //first collect information from the exe in question
+            var bytes = File.ReadAllBytes(exePath);
+            var ms = new MemoryStream(bytes);
+            var br = new BinaryReader(ms);
+            
+            ms.Seek(0x3C, SeekOrigin.Begin);
+            var ntHeaderOffset = br.ReadInt32();
+
+            ms.Seek(ntHeaderOffset, SeekOrigin.Begin);
+            var peSignature = new string(br.ReadChars(4));
+            if (peSignature != "PE\0\0")
+                return (false, "Invalid PE header signature");
+
+            ms.Seek(2, SeekOrigin.Current); //jump over IMAGE_MACHINE enum
+            var sectionCount = br.ReadUInt16();
+
+            ms.Seek(12, SeekOrigin.Current);
+            var optionalSectionSize = br.ReadUInt16();
+            var characteristics = br.ReadUInt16();
+
+            //we're in the optional header now
+            ms.Seek(28, SeekOrigin.Current);
+            var imageBase = br.ReadUInt32();
+            var sectionAlignment = br.ReadUInt32();
+            ms.Seek(34, SeekOrigin.Current);
+            var dllCharacteristics = br.ReadUInt16();
+
+            //jump to section headers
+            var sectionHeaderOffset = ntHeaderOffset + 24 + optionalSectionSize;
+
+            var sections = new List<ExeSection>();
+
+            for (var i = 0; i < sectionCount; i++)
+            {
+                ms.Seek(sectionHeaderOffset + 40 * i, SeekOrigin.Begin);
+                var section = new ExeSection
+                {
+                    Name = new string(br.ReadChars(8)).Trim('\0'),
+                    VSize = br.ReadInt32(),
+                    VOffset = br.ReadInt32(),
+                    RSize = br.ReadInt32(),
+                    ROffset = br.ReadInt32(),
+                };
+                ms.Seek(12, SeekOrigin.Current);
+                section.Flags = br.ReadUInt32();
+
+                var code = section.Name.ToUpper();
+                if (code.StartsWith("."))
+                    code = code.Substring(1);
+                while (sections.Any(s => s.Code == code)) //can happen if one section is .text and there's also a text section (no .)
+                    code = "_" + code;
+                section.Code = code;
+
+                sections.Add(section);
+            }
+
+            var hasRelocations = sections.Any(s => s.Name.Contains("reloc"));
+
+            var lastSection = sections[sectionCount - 1];
+
+            var patchSection = new ExeSection()
+            {
+                Name = ".patch",
+                Code = "PATCH",
+                VSize = 0x10000,
+                RSize = 0x10000,
+                VOffset = RoundToAlignment(lastSection.VOffset + lastSection.VSize, (int)sectionAlignment),
+                ROffset = RoundToAlignment(lastSection.ROffset + lastSection.RSize, (int)sectionAlignment),
+            };
+            sections.Add(patchSection);
+
+            //we have the data we need, now lets generate our patch file!
+
+            var patchText = new List<string>();
+            var exeName = Path.GetFileName(exePath);
+
+            patchText.Add("format binary as 'exe'");
+            patchText.Add("use32");
+            patchText.Add("");
+            patchText.Add("include 'patchmacros.inc'");
+            patchText.Add("");
+            patchText.Add($"patchfile '{exeName}'");
+            patchText.Add("");
+            patchText.Add(";--------------------------------------------");
+            patchText.Add("; Addressing stuff");
+            patchText.Add(";--------------------------------------------");
+            patchText.Add("");
+            patchText.Add($"IMAGE_BASE = 0x{imageBase:X8}");
+
+            foreach (var s in sections)
+            {
+                patchText.Add($"{s.Code}_VOFFSET = 0x{s.VOffset:X8}");
+                patchText.Add($"{s.Code}_ROFFSET = 0x{s.ROffset:X8}");
+            }
+
+            patchText.Add("");
+
+            foreach (var s in sections)
+            {
+                patchText.Add($"{s.Code}_ORG = IMAGE_BASE + {s.Code}_VOFFSET - {s.Code}_ROFFSET");
+            }
+
+            patchText.Add("");
+            patchText.Add($"PATCH_VSIZE = (patch_physical_size + 0x{(sectionAlignment - 1):X}) / 0x{sectionAlignment:X4} * 0x{sectionAlignment:X4}");
+            patchText.Add($"IMAGE_SIZE = PATCH_VOFFSET + PATCH_VSIZE");
+            patchText.Add($"PE_LOCATION = 0x{ntHeaderOffset:X4}");
+
+            patchText.Add("");
+            patchText.Add(";--------------------------------------------");
+            patchText.Add("; Patch the header!");
+            patchText.Add(";--------------------------------------------");
+            patchText.Add("");
+            if (hasRelocations)
+            {
+                patchText.Add(
+                    ";Relocation stuff is commented out by default. Uncomment them to (try) to patch out relocations.");
+                patchText.Add("");
+            }
+            patchText.Add("patchsection IMAGE_BASE ; === PE header ===");
+            patchText.Add("");
+            patchText.Add("patchatfixed PE_LOCATION + 6 ; Update number of sections");
+            patchText.Add($"\tdw {sections.Count}");
+            patchText.Add("");
+            if (hasRelocations)
+            {
+                patchText.Add($";patchatfixed PE_LOCATION + 0x16 ;disable relocations");
+                patchText.Add($";\tdw 0x{(characteristics | 1):X4}");
+                patchText.Add("");
+            }
+            patchText.Add("patchatfixed PE_LOCATION + 0x50 ; Update size of image");
+            patchText.Add("\tdd IMAGE_SIZE");
+            patchText.Add("");
+            if (hasRelocations)
+            {
+                patchText.Add($";patchatfixed PE_LOCATION + 0x5E ;disable dynamic base");
+                patchText.Add($";\tdw 0x{(dllCharacteristics | 0x40):X4}");
+                patchText.Add("");
+            }
+            patchText.Add($"patchatfixed PE_LOCATION + 0x18 + 0x{optionalSectionSize:X} + 0x28 * {sections.Count - 1}  ;Add .patch section");
+            patchText.Add("\tdd '.pat','ch'           ; Name");
+            patchText.Add("\tdd PATCH_VSIZE           ; Virtual size");
+            patchText.Add("\tdd PATCH_VOFFSET         ; VOffset");
+            patchText.Add("\tdd patch_physical_size   ; Physical size");
+            patchText.Add("\tdd PATCH_ROFFSET         ; Physical offset");
+            patchText.Add("\tdd 0,0,0                 ; Unused");
+            patchText.Add("\tdd 0xE0000060            ; Attributes");
+            patchText.Add("");
+            patchText.Add(";--------------------------------------------");
+            patchText.Add("; Patching!");
+            patchText.Add(";--------------------------------------------");
+            patchText.Add("");
+
+            foreach (var s in sections)
+            {
+                if (s.Code == "PATCH")
+                {
+                    patchText.Add(";--------------------------------------------");
+                    patchText.Add("; Patch Section");
+                    patchText.Add(";--------------------------------------------");
+                    patchText.Add("");
+                }
+
+                patchText.Add($"patchsetsection {s.Code}_ORG, {s.Code}_ROFFSET");
+                patchText.Add("");
+            }
+            patchText.Add("patch_section_start:");
+            patchText.Add("");
+            patchText.Add("");
+            patchText.Add(";put your own code here!");
+            patchText.Add("");
+            patchText.Add("");
+            patchText.Add("db 0");
+            patchText.Add("");
+            patchText.Add("patch_section_end:");
+            patchText.Add("patch_physical_size = patch_section_end - patch_section_start");
+            patchText.Add("");
+            patchText.Add("patchend");
+
+            File.WriteAllLines(asmPath, patchText);
+
+            return (true, null);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: ExePatchPreparer.exe exename.exe");
+                return;
+            }
+            var file = args[0];
+
+            var basename = Path.GetFileNameWithoutExtension(file);
+            var ext = Path.GetExtension(file);
+            var fullpath = Path.GetDirectoryName(file);
+            var patchname = $"{basename}patch";
+
+            var asmpatch = Path.Combine(fullpath, patchname + ".asm");
+
+            var res = CreateExePatchFile(file, asmpatch);
+            if (!res.success)
+            {
+                Console.WriteLine($"Error: {res.error}");
+                return;
+            }
+
+            var bat = new List<string>();
+            bat.Add($"del \"{patchname}.exe\"");
+            bat.Add($"FASM.exe \"{patchname}.asm\"");
+            bat.Add("pause");
+
+            File.WriteAllLines(Path.Combine(fullpath, "build.bat"), bat);
+
+            var macropath = Path.Combine(fullpath, "patchmacros.inc");
+
+            if (!File.Exists(macropath))
+            {
+                var stream = Assembly.GetExecutingAssembly()
+                    .GetManifestResourceStream("ExePatchPreparer.patchmacros.inc");
+
+                using (var macrostream = new FileStream(macropath, FileMode.Create))
+                {
+                    stream.CopyTo(macrostream);
+                    macrostream.Close();
+                }
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # ExePatchPreparer
+
+This utility does the busy work in creating the base for a simple an exe patch, getting all the offsets and macros needed and creating a FASM .asm file. You can place your own modifications in the .asm file and compile it to apply the patches.
+
+## Basic Usage
+
+Simply run the tool with the name of your exe (or drag the exe onto the tool) to have the .asm patch file created. You can then modify the .asm file yourself and run the provided simple batch file to execute, or call FASM directly. You'll need the FASM assembler added to your path variables for the batch file to run.
+
+Once you have a .asm file, you'll need to use a tool such as Ghidra and IDA to find addresses you wish to patch. From there, you can place the changes you want into the assembly file.
+
+### Calling your own function
+
+Say you have found a location within an executable where you wish to inject your own code. The example here patches a stretch of code from addresses 0x4af51b to 0x4af521 with a call to our own function. You'd place this code in your .asm file below the patchsetsection for the section the code you're patching resides in. Usually that's the .text section, but it can vary.
+```
+patchat 0x4af51b
+	call YourCustomFunction
+patchtill 0x4af521
+```
+The `patchat` and `patchtill` macros marks the segment you wish to overwrite. If the code you provide is shorter than the segment in question, `NOP` instructions will be placed to fill the remaining space. You'll get an error if your code is longer than the space available. 
+
+You'll also need to take care that you aren't breaking the stack or registers that the function you're patching expects to work. One option would be to use a `jmp` to your code and then use a label to return to, so you can jump to your code without affecting the stack, like so:
+```
+patchat 0x4af51b
+	jmp YourCustomFunction
+YourFunctionReturn:
+patchtill 0x4af521
+```
+You can place your own code within the created patch section, after the patch_section_start label.
+```
+YourCustomFunction:
+	; just regular fasm assembly, whatever you want goes here
+	add dword [ecx+4],2 ;just some random example
+	retn
+```
+If you're using a jmp to access your function, you'd want to replace the return with a jmp back to your label.
+### Handy macros
+
+In addition to `patchat` and `patchtill`, there are a number of macros designed to quickly patch certain values. Those are:
+
+```
+quickpatchpush address, target ; patches a simple push operation (5 bytes in length) to your own value
+quickpatchbyte address, value ; patches a single byte at a specific address
+quickpatchshort address, value ; patches a word size value at a specific address
+quickpatchint address, value ; patches a dword size value at a specific address
+quickpatchlong address, value ; patches a qword size value at a specific address
+```
